25dbaebcc8a4013da7d0c74f7f89e56376955eb7
kcjpop <kcj.pop.com@gmail.com>
2013-04-20 19:32:03 +0300

    Use Asset to manage JavaScript and CSS.

--- application/controllers/admin/base.php
+++ application/controllers/admin/base.php
@@ -2,4 +2,16 @@
 class Admin_Base_Controller extends Base_Controller
 {
 	public $layout = 'layouts.admin';
+
+	public function before()
+	{
+		// JavaScript
+		Asset::add('bootstrap', 'js/bootstrap.min.js');
+
+		// CSS
+		Asset::add('bootstrap', 'css/bootstrap.min.css');
+		Asset::add('bootstrap-responsive', 'css/bootstrap-responsive.css');
+		Asset::add('font-awesome', 'css/font-awesome.min.css');
+		Asset::add('admin', 'css/admin.style.css');
+	}
 }
--- application/views/layouts/admin.php
+++ application/views/layouts/admin.php
@@ -4,10 +4,7 @@
     <title>cafe.ehkoo.com - Browse</title>
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../public/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../../public/css/bootstrap-responsive.css">
-    <link rel="stylesheet" type="text/css" href="../../public/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="../../public/css/admin.style.css">
+    <?php echo Asset::styles() ?>
 </head>
 <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
@@ -36,7 +33,7 @@
 
     <!-- /container -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script type="text/javascript" src="../../public/js/bootstrap.min.js"></script>
+    <?php echo Asset::scripts() ?>
 </body>
 </html>
 

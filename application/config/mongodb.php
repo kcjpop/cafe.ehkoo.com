fd32d17059925eb334d0dfeb6faaafcf561dbdd5
kcjpop <kcj.pop.com@gmail.com>
2013-05-05 00:26:58 +0300

    Modify the database config to match 2 environments.

--- application/config/mongodb.php
+++ application/config/mongodb.php
@@ -1,5 +1,24 @@
 <?php
+foreach(array('OPENSHIFT_MONGODB_DB_HOST',
+	'OPENSHIFT_MONGODB_DB_PORT',
+	'OPENSHIFT_MONGODB_DB_USERNAME',
+	'OPENSHIFT_MONGODB_DB_PASSWORD') as $env_field)
+{
+	// Local environment
+	if(!isset($_ENV[$env_field]))
+	{
+		return array(
+			'connection_string' => 'mongodb://localhost:27017',
+			'database'          => 'cafevn'
+		);		
+	}
+}
+
 return array(
-	'connection_string' => 'mongodb://localhost:27017',
+	'connection_string' => sprintf('mongodb://%s:%s@%s:%s',
+							$_ENV['OPENSHIFT_MONGODB_DB_USERNAME'],
+							$_ENV['OPENSHIFT_MONGODB_DB_PASSWORD'],
+							$_ENV['OPENSHIFT_MONGODB_DB_HOST'],
+							$_ENV['OPENSHIFT_MONGODB_DB_PORT']),
 	'database'          => 'cafevn'
 );
